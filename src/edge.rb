ca5305491642f78b4eeac7211a84d60eea597e8e
MuhammetDilmac <iletisim@muhammetdilmac.com.tr>
2022-05-27 14:56:30 +0300

    Adding neighbor when edge created

--- src/edge.rb
+++ src/edge.rb
@@ -4,12 +4,19 @@
 class Edge
   attr_reader :weight, :from, :to
 
+  # @param [Node] from
+  # @param [Node] to
+  # @param [Integer] weight
   def initialize(from:, to:, weight: 0)
     @from       = from
     @to         = to
     @weight     = weight
+
+    from.neighbors.push(to) unless from.neighbors.include?(to)
+    to.neighbors.push(from) unless to.neighbors.include?(from)
   end
 
+  # @param [Integer] weight
   def increment_weight!(weight: 1)
     @weight += weight
   end
